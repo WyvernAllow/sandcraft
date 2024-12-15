c836dda839203227a565a7e61a9c22876ab00e7e
WyvernAllow <bloodflame2535@gmail.com>
2024-12-15 22:15:58 +0200

    Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,57 @@
+#include <spdlog/spdlog.h>
+
+#include <cstdlib>
 #include <iostream>
 
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+
+static void glfw_error_callback(int error_code, const char* description) {
+    spdlog::error("GLFW: {}", description);
+}
+
+static void framebuffer_size_callback(GLFWwindow* window, int width,
+                                      int height) {
+    glViewport(0, 0, width, height);
+}
+
 int main(int argc, char** argv) {
-    std::cout << "Hello, World!\n";
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) {
+        spdlog::critical("glfwInit failed");
+        return EXIT_FAILURE;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window =
+        glfwCreateWindow(800, 450, "Sandcraft", nullptr, nullptr);
+    if (!window) {
+        spdlog::critical("glfwCreateWindow failed");
+        return EXIT_FAILURE;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        spdlog::critical("gladLoadGL failed");
+        return EXIT_FAILURE;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
