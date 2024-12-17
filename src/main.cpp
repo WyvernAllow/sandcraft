32081f895e175870b6de39d783a8bc8a6de1e1c6
WyvernAllow <bloodflame2535@gmail.com>
2024-12-17 17:25:23 +0200

    OpenGL version 4.3

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
     }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     GLFWwindow* window =
         glfwCreateWindow(800, 450, "Sandcraft", nullptr, nullptr);
